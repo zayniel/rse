6efba3f29a1d33d9eae684eed3b2ba2e1345692b
zayniel <98770698+zayniel@users.noreply.github.com>
2023-09-10 17:15:26 -0400

    tried updating favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
