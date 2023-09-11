a7f53f167f13b755102994093ae7564eb8db0c8b
Zayne <98770698+zayniel@users.noreply.github.com>
2023-09-11 11:09:08 -0400

    +local images to fix rendering on some browsers

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -6,11 +6,11 @@ export default function NavBar() {
         <section className='container'>
             <div className='slider-wrapper'>
                 <div className='slider'>
-                    <img id='slide-1' src='https://drive.google.com/uc?export=view&id=1nAuLBXi4UTqFUZz-Jp7mEilsZlGl6hml' alt='group-with-car'/>
-                    <img id='slide-2' src='https://drive.google.com/uc?export=view&id=1dB4DE9MlSAmeCPfftq47iZN-CYjolTTK' alt='placeholder1'/>
-                    <img id='slide-3' src='https://drive.google.com/uc?export=view&id=1753UGUBDVHWK7UtYUJfb0Im8_K4xyaG2' alt='placeholder2'/>
-                    <img id='slide-4' src='https://drive.google.com/uc?export=view&id=1e9P0UMOXs3TkOXjdfcjgCiW5O7h8pQYv' alt='placeholder3'/>
-                    <img id='slide-5' src='https://drive.google.com/uc?export=view&id=1qtLVbPiKDKqWfg5XJGPEC0mgZFKu2AuK' alt='placeholder4'/>
+                    <img id='slide-1' src='./group.jpg' alt='group-with-car'/>
+                    <img id='slide-2' src='./car-side.jpg' alt='placeholder1'/>
+                    <img id='slide-3' src='./looking-at-monitor.jpg' alt='placeholder2'/>
+                    <img id='slide-4' src='./car-front.jpg' alt='placeholder3'/>
+                    <img id='slide-5' src='./logan-chaz.jpg' alt='placeholder4'/>
                 </div>
                 <div className='slider-nav'>
                     <a href='#slide-1'></a>
