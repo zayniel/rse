960074e693986f22418ec35921b7239761f42345
zayniel <98770698+zayniel@users.noreply.github.com>
2023-09-22 10:30:41 -0400

    thumbnail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider>
       <Head>
         <title>Robotic Systems Enterprise</title>
+        <meta property='og:image' content='./logo-title.png'/>
         <meta name='author' content='Zayne Pepin'/>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         <meta name='description' content='Informational Site for Robotic Systems Enterprise at Michigan Technological University'/>
