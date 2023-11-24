b7ae0839a831c930553cdbdf3a68c6d73d5407c0
Zayne <98770698+zayniel@users.noreply.github.com>
2023-11-24 14:58:56 -0500

    created baseplate team page

---
+++ pages/teams.tsx
@@ -0,0 +1,101 @@
+import Head from 'next/head';
+import * as React from 'react';
+import NavBar from '../components/NavBar'
+import Slideshow from '../components/Slideshow'
+import {
+  ChakraProvider,
+  extendTheme,
+  Box,
+  LightMode,
+} from '@chakra-ui/react'
+import "@fontsource/blinker"
+import { BsInstagram, BsFacebook } from 'react-icons/bs'
+import { RiTwitterXFill } from 'react-icons/ri'
+
+const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: 'white'
+      }
+    })
+  },
+  components: {
+    Text: {
+      variants: {
+        'title': {
+          fontFamily: 'Blinker'
+        },
+        'sub-title': {
+          fontFamily: 'Blinker'
+        },
+      }
+    },
+  },
+  breakpoints: {
+    xs: '650px',
+    sm: '1200px',
+    md: '1320px',
+    lg: '1600px'
+  }
+})
+
+export default function Home() {
+  return (
+    <ChakraProvider theme={theme}>
+      <LightMode>
+      <NavBar/>
+      <div className='image-container'>
+        <img className='image' src='./teams.jpg'></img>
+      </div>
+      <div className='main-container'>
+        <div className='bio-container'>
+          <h4><b>Teams</b></h4>
+          <hr className='new1'></hr>
+          <p>The Robotic Systems Enterprise is made up of various sub teams which work in parallel to complete large scale goals.</p>
+          <hr className='new2'></hr>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="card">Build</div>
+        <div className="card">Perception</div>
+        <div className="card">Mapping & Planning</div>
+        <div className="card">Controls</div>
+        <div className="card">Simulation</div>
+        <div className="card">Innovation: Congestion</div>
+        <div className="card">Innovation: Accessibility</div>
+      </div>
+      <div className='footer'>
+        <div className='footer-container'>
+          <div className='footer-explore'>
+            <h5>Explore</h5>
+            <hr className='new3'/>
+            <a href='https://rse-mtu.vercel.app'>Home</a>
+            <a href='/autodrive'>AutoDrive II</a>
+            <a href='/projects'>Projects</a>
+            <a href='/teams'>Teams</a>
+            <a href='/contact'>Contact Us</a>
+          </div>
+          <div className='footer-location'>
+            <h5>Location</h5>
+            <hr className='new4'/>
+            <div>Electrical Energy Resources Center Lab 809, Houghton, MI 49931</div>
+          </div>
+          <div className='footer-social'>
+            <h5>Socials</h5>
+            <hr className='new5'/>
+            <div className='social-container'>
+              <a href='https://www.instagram.com/mturobotics/' target='_blank' rel='noopener noreferrer'><button className='social'><BsInstagram/></button></a>
+              <a href='https://twitter.com/MTUAutoDrive/' target='_blank' rel='noopener noreferrer'><button className='social'><RiTwitterXFill/></button></a>
+              <a href='https://m.facebook.com/MichiganTechRoboticSystemsEnterprise/' target='_blank' rel='noopener noreferrer'><button className='social'><BsFacebook/></button></a>
+            </div>
+          </div>
+        </div>
+        <div className='copyright'>
+            © 2023 Michigan Technological University
+        </div>
+      </div>
+      </LightMode>
+    </ChakraProvider>
+  );
+}
